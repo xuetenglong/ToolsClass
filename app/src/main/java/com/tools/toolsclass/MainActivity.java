421baee3ac6d630c649839f225f9c5f076fc63b2
duanjie <xuetenglongyeah@163.com>
2019-10-09 10:29:23 +0800

    init

--- app/src/main/java/com/tools/toolsclass/MainActivity.java
+++ app/src/main/java/com/tools/toolsclass/MainActivity.java
@@ -1,10 +1,12 @@
 package com.tools.toolsclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
